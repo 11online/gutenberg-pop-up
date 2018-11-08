74f55ea4a33c9d03ffcf57c29a6f5b6d1d3d7cc2
Eric Debelak <ericdebelak@gmail.com>
2018-11-08 11:57:17 -0700

    FIXED: wp coding standards changes

--- plugin.php
+++ plugin.php
@@ -1,15 +1,15 @@
 <?php
-/**	
- * Plugin Name: Pop Up Block	
- * Plugin URI: https://11online.us/	
- * Description: Gutenberg Pop Up — is a Gutenberg plugin created via create-guten-block.	
- * Author: ericdebelak	
- * Author URI: https://11online.us/	
- * Version: 1.0.0	
- * License: GPL2+	
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt	
- *	
- * @package CGB	
+/**
+ * Plugin Name: Pop Up Block
+ * Plugin URI: https://11online.us/
+ * Description: Gutenberg Pop Up — is a Gutenberg plugin created via create-guten-block.
+ * Author: ericdebelak
+ * Author URI: https://11online.us/
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package CGB
  */
 
 // Exit if accessed directly.
